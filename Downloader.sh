937b53ad2db94b0eac777ce33e0c3a651e540b70
The12Forest <manuel@steka.ch>
2025-10-22 12:49:39 +0200

    Link update

--- Downloader.sh
+++ Downloader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # downloader.sh
 
-curl -fsSL https://tinyurl.com/2hftnvhs -o setup-docker.sh
+curl -fsSL https://raw.githubusercontent.com/The12Forest/BLJ-Docker-SQL-installer/refs/heads/main/Docker%20and%20SQL.sh -o setup-docker.sh
 bash setup-docker.sh
